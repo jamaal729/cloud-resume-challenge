52259565a4f2cce60e4c36303c4e632763ed1f6d
Jamaal Ahmed <jamaal729@yahoo.com>
2025-11-30 07:09:10 +0000

    Implement page view count with simple express backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import ResumeData from './data/ResumeData';
 import HeaderBlock from './components/HeaderBlock';
 import SectionsBlock from './components/SectionsBlock';
+import FooterBlock from './components/FooterBlock';
 
 function App() {
   return (
-    <div className="page">
-      <HeaderBlock data={ResumeData} />
-      <SectionsBlock data={ResumeData} />
-    </div>
+    <>
+      <div className="page">
+        <HeaderBlock data={ResumeData} />
+        <SectionsBlock data={ResumeData} />
+        <br />
+        <FooterBlock data={ResumeData} />
+      </div>
+    </>
   );
 }
 
